e1f14f7759b0976ea41a811b2629eea45ff84c3e
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-03-25 21:01:07 -0300

    radar animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -4,15 +4,26 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     var body: some View {
         Button("Tap me!") {
-            animationAmount += 1
+            // animationAmount += 1
         }
         .padding(50)
         .background(.blue)
         .foregroundStyle(Color.white)
         .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(.default, value: animationAmount)
+        .overlay(
+            Circle()
+                .stroke(.blue)
+                .scaleEffect(animationAmount)
+                .opacity(1.5 - animationAmount)
+                .animation(
+                    .easeOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+        )
+        .onAppear {
+            animationAmount = 1.5
+        }
     }
 }
 
