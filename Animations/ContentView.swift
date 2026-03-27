984786f27f7b0dd1bb4f7088534b938584a3013b
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-03-26 21:14:45 -0300

    Transitions

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -3,27 +3,23 @@ import SwiftUI
 struct ContentView: View {
     let letters = Array("Hello SwiftUI")
     @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
+
     
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count, id: \.self) { num in
-                Text(String(letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(enabled ? .blue : .red)
-                    .offset(dragAmount)
-                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
-            }
-        }
-        .gesture(
-            DragGesture()
-                .onChanged { dragAmount = $0.translation }
-                .onEnded { _ in
-                    dragAmount = .zero
+        VStack{
+            Button("Tap me") {
+                withAnimation {
                     enabled.toggle()
                 }
-        )
+            }
+            .foregroundStyle(.black)
+            if enabled {
+                Rectangle()
+                    .fill(.green)
+                    .frame(width: 200, height: 200)
+                    .transition(AsymmetricTransition(insertion: .scale, removal: .opacity))
+            }
+        }
     }
 }
 
